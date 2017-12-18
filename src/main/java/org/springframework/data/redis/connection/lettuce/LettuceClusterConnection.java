1602e1b00cfeeb18ecd0306d574c13a8d849a4e5
Mark Paluch <mpaluch@pivotal.io>
2017-12-18 10:15:45 +0100

    DATAREDIS-731 - Polishing.
    Document undocumented parameter. Fix typo.

--- src/main/java/org/springframework/data/redis/connection/lettuce/LettuceClusterConnection.java
+++ src/main/java/org/springframework/data/redis/connection/lettuce/LettuceClusterConnection.java
@@ -136,6 +136,7 @@ public class LettuceClusterConnection extends LettuceConnection implements Defau
 	 *
 	 * @param connectionProvider must not be {@literal null}.
 	 * @param executor must not be {@literal null}.
+	 * @param timeout must not be {@literal null}.
 	 * @since 2.0
 	 */
 	public LettuceClusterConnection(LettuceConnectionProvider connectionProvider, ClusterCommandExecutor executor,
